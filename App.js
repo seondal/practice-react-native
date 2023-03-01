031200c2a180022489074363921aeecebc024d7b
seondal <sseondal@gmail.com>
2023-03-01 19:06:52 +0900

    #2.8 Weather

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Dimensions, ScrollView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import * as Location from "expo-location";
+
+const API_KEY = "b316c980441d737b14db685d96bfe1f5";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   const [ok, setOk] = useState(true);
   const [city, setCity] = useState("Loading...");
+  const [weathers, setWeathers] = useState([]);
 
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -21,10 +31,15 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setWeathers(json.list);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
   return (
@@ -39,34 +54,28 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         Style={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
+        {weathers.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" />
+          </View>
+        ) : (
+          weathers.map(
+            (weather, idx) =>
+              idx % 8 == 0 && (
+                <View style={styles.day} key={idx}>
+                  <Text style={styles.temp}>
+                    {parseFloat(weather.main.temp).toFixed(1)}
+                  </Text>
+                  <Text style={styles.description}>
+                    {weather.weather[0].main}
+                  </Text>
+                  <Text style={styles.tinyText}>
+                    {weather.weather[0].description}
+                  </Text>
+                </View>
+              )
+          )
+        )}
       </ScrollView>
     </View>
   );
@@ -80,20 +89,32 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: {
-    fontSize: 60,
-    fontWeight: "600",
+    fontSize: 58,
+    fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
-    marginTop: 20,
-    fontSize: 178,
+    marginTop: 50,
+    fontWeight: "600",
+    fontSize: 100,
+    color: "white",
   },
   description: {
-    marginTop: -20,
-    fontSize: 60,
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500",
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "400",
   },
 });
